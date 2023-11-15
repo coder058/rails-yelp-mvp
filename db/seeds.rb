e743ac77ed2facda2ea3771f379750bbfea38e69
jordi perez <jlpmccs@gmail.com>
2023-11-15 22:46:35 +0100

    terminado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+Restaurant.destroy_all
+
+5.times do |i|
+  Restaurant.create!(
+    name: "Restaurant #{i + 1}",
+    address: "#{i + 1} Example Street, City",
+    phone_number: "123-456-7890",
+    category: %w[chinese italian japanese french belgian].sample
+  )
+end
+
+puts "Created #{Restaurant.count} restaurants."
